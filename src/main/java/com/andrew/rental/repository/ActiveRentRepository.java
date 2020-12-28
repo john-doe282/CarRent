55ef07553750bfbface5610feba9beeab16224b7
john-doe282 <molnar2@icloud.com>
2020-12-29 00:18:36 +0200

    rent fix

--- src/main/java/com/andrew/rental/repository/ActiveRentRepository.java
+++ src/main/java/com/andrew/rental/repository/ActiveRentRepository.java
@@ -16,9 +16,5 @@ import java.util.List;
 @SuppressWarnings("unused")
 @Repository
 public interface ActiveRentRepository extends JpaRepository<ActiveRent, Long> {
-
-//    @Query("select activeRent from ActiveRent activeRent where activeRent.client.login = ?#{principal.username}")
-//    List<ActiveRent> findByClientIsCurrentUser();
-
     Page<ActiveRent> findAllByClient(Pageable pageable, User client);
 }
